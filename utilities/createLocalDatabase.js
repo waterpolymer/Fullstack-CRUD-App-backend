5d52439a9695dcea224623ddac5c6bb05162c0d2
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-16 11:36:52 -0500

    fix postgres, creates db on start

--- utilities/createLocalDatabase.js
+++ utilities/createLocalDatabase.js
@@ -1,29 +1,28 @@
-const pgtools = require('pgtools');
+const pgtools = require("pgtools");
 
 // An object with user, host, port, and password properties;
 const config = {
-  user: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  password: process.env.LOCAL_DATABASE_PASSWORD
+	user: "postgres",
+	host: "localhost",
+	port: process.env.PORT
 };
 
 // The name of the database to create;
-const databaseName = require('./databaseName');
+const databaseName = require("./databaseName");
 
 // A callback that takes an error argument;
 // If cb is omitted the function will return a Promise;
 const cb = (err, res) => {
-  console.log(`Attempting to create the database: ${databaseName}!`);
+	console.log(`Attempting to create the database: ${databaseName}!`);
 
-  if (err) {
-    console.error(err);
-    process.exit(-1);
-  }
+	if (err) {
+		console.error(err);
+		process.exit(-1);
+	}
 
-  console.log(res);
-  console.log(`Successfully created the database: ${databaseName}!`);
-}
+	console.log(res);
+	console.log(`Successfully created the database: ${databaseName}!`);
+};
 
 const createLocalDatabase = () => pgtools.createdb(config, databaseName, cb);
 
