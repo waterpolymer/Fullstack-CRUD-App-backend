4180400c2b735676146bf8351ba828467132dc5f
Kun Yu <kun.yu25@myhunter.cuny.edu>
2020-01-16 15:58:56 -0500

    Revert "removed getitemsbyId"
    This reverts commit c09f5b4dcb2554d04e6760253e148a42c06fca20.

--- controllers/campuses.js
+++ controllers/campuses.js
@@ -3,6 +3,7 @@ const { Campus, Student } = require("../database/models");
 const campusController = {
 	getAllCampuses: getAllCampuses,
 	addCampus: addCampus,
+	getCampusById: getCampusById,
 	updateCampusById: updateCampusById,
 	deleteCampusById: deleteCampusById
 };
@@ -22,6 +23,20 @@ async function addCampus(req, res, next) {
 	try {
 		const newCampus = await Campus.create(req.body);
 		res.status(201).json(newCampus);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+// GET api/campuses/:id
+async function getCampusById(req, res, next) {
+	try {
+		const campus = await Campus.findByPk(req.params.id, { include: [Student] });
+		if (campus) {
+			res.status(200).json(campus);
+		} else {
+			res.status(404).send("Campus not found.");
+		}
 	} catch (err) {
 		console.log(err);
 	}
