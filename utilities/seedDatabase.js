66618c072834708800ab61fd8058ada97cddafc3
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2020-01-16 13:26:14 -0500

    set up models

--- utilities/seedDatabase.js
+++ utilities/seedDatabase.js
@@ -1,22 +1,36 @@
-const { Student } = require("../database/models");
+const { Student, Campus } = require("../database/models");
 
 const seedDatabase = async () => {
 	await Promise.all([
 		Student.create({
 			firstName: "Kun",
-			lastName: "Yu"
+			lastName: "Yu",
+			image_URL: "https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg",
+			email: "kun@gmail.com"
 		}),
 		Student.create({
 			firstName: "Jordan",
-			lastName: "Yaqoob"
+			lastName: "Yaqoob",
+			image_URL: "https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg",
+			email: "jordan@gmail.com"
 		}),
 		Student.create({
 			firstName: "Kendrick",
-			lastName: "Shao"
+			lastName: "Shao",
+			image_URL: "https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg",
+			email: "kendrick@gmail.com"
 		}),
 		Student.create({
 			firstName: "Sami",
-			lastName: "Beig"
+			lastName: "Beig",
+			image_URL: "https://www.jennstrends.com/wp-content/uploads/2013/10/bad-profile-pic-2-768x768.jpeg",
+			email: "sami@gmail.com"
+		}),
+		Campus.create({
+			name: "Hunter College",
+			email: "hunter@gmail.com",
+			location: "NYC, NY",
+			image_URL: "https://s29068.pcdn.co/wp-content/uploads/hunter-new-york-city-street-view.jpg"
 		})
 	]);
 };
