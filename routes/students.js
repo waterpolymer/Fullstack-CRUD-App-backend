20d61a5a5d6c6c2b4f78bbfadba92c06d12b0e7b
Jordan Yaqoob <jordanyaqoob2@gmail.com>
2020-01-16 14:29:19 -0500

    completed and tested all five student routes

--- routes/students.js
+++ routes/students.js
@@ -6,6 +6,12 @@ const studentController = require("../controllers/students");
 router.route("/").get(studentController.getAllStudents);
 // POST api/students/
 router.route("/").post(studentController.addStudent);
+// GET api/students/:id
+router.route("/:id").get(studentController.getStudent);
+// PUT api/students/:id
+router.route("/:id").put(studentController.putStudent);
+// DELETE api/students/:id
+router.route("/:id").delete(studentController.deleteStudent);
 
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
